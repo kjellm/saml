a07438a11b24295f403a6dc7084ab237b685ef0a
Kjell-Magne Øierud <kjellm@oierud.net>
2012-06-08 10:15:22 +0200

    Add a XML namespace for metadata

---
+++ lib/saml/metadata/xml_namespaces.rb
@@ -0,0 +1,18 @@
+# XML Namespaces
+# 
+# SAML v2.0 Metadata
+# 
+# Section FIXME
+#
+# http://docs.oasis-open.org/security/saml/v2.0/saml-metadata-2.0-os.pdf
+#
+
+module SAML
+  module Metadata
+    
+    XMLNamespaces = {
+      :md  => 'urn:oasis:names:tc:SAML:2.0:metadata',
+    }
+
+  end
+end
