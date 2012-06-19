b65eb838d5cd21becd627e475387ec910d067721
Kjell-Magne Øierud <kjellm@oierud.net>
2012-06-19 23:51:34 +0200

    Validate the XML Signature on responses

--- lib/saml/core/response.rb
+++ lib/saml/core/response.rb
@@ -1,20 +1,26 @@
+require 'xml_signature'
+
 module SAML
   module Core
     class Response < StatusResponse
 
-      attr_accessor :assertions
+      attr_reader :assertions
 
-      def self.from_xml(xml)
-        res = super(xml)
-        
-        assertions = xml.get_elements('saml:Assertion')
-        unless assertions.empty?
-          res.assertions = assertions.map do |a|
-            Assertion.from_xml(a)
-          end
+      def self.from_xml(xml); new.from_xml(xml); end
+
+      def from_xml(xml)
+        super(xml)
+        @xml = xml
+
+        @assertions = xml.get_elements('saml:Assertion').map do |a|
+          Assertion.from_xml(a)
         end
         
-        res
+        self
+      end
+
+      def valid?(expected_certificate)
+        XMLSignature.new(@xml).verify(expected_certificate)
       end
 
     end
