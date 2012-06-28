cb88d3e0cc61915cd987dd895264e884605688e2
Kjell-Magne Øierud <kjellm@oierud.net>
2012-06-28 20:18:46 +0200

    Add SessionIndex to LogoutRequest

--- lib/saml/core/logout_request.rb
+++ lib/saml/core/logout_request.rb
@@ -3,6 +3,7 @@ module SAML
     class LogoutRequest < RequestAbstract
 
       attr_accessor :name_id
+      attr_accessor :session_index
       
       def xml_document
         xml = Document.new
@@ -15,6 +16,11 @@ module SAML
         unless @name_id.nil?
           name_id_node = xml.root.add_element("saml:NameID")
           name_id_node.text = @name_id
+        end
+
+        unless @session_index.nil?
+          session_index_node = xml.root.add_element("samlp:SessionIndex")
+          session_index_node.text = @session_index
         end
         
         xml
